ecd6bd167e2d1586aa499f3892b356b9583e4cf0
Jin Nakano <jin.nakano.canada@gmail.com>
2024-07-19 17:01:21 -0700

    update htmls

---
+++ js/contact/home-modal.js
@@ -0,0 +1,21 @@
+function openModal1() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("youtubeVideo").src = "https://www.youtube.com/embed/jNQXAC9IVRw?si=Tpj-Qq2SyutbmUUF";
+}
+function openModal2() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("youtubeVideo").src = "https://www.youtube.com/embed/3KtWfp0UopM?si=4iPBBkoMCQ1OOXJq";
+}
+function openModal3() {
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("youtubeVideo").src = "https://www.youtube.com/embed/4WXs3sKu41I?si=TZ9WoLJAldIKcM-5";
+}
+
+
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  document.getElementById("youtubeVideo").src = "";
+}
+
+
